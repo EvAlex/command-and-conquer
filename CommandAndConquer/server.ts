d0e420e51fe330cf3fb8899e5a2c76a718464ee4
EvAlex <evalexhimself@gmail.com>
2015-10-24 23:24:44 +0300

    created NodeJS project, set it up for NTVS build, moved all content there, hosted it as static.

---
+++ CommandAndConquer/server.ts
@@ -0,0 +1,15 @@
+﻿import http = require('http');
+import express = require('express');
+import logger = require('winston');
+
+var port = process.env.port || 1337,
+    app = express();
+
+app.use(express.static('./Web'));
+var server = app.listen(port, 'localhost', () => {
+    var address = server.address(),
+        host = address.address,
+        port = address.port;
+
+    logger.info(`Server is listening at http://${host}:${port}`);
+});
