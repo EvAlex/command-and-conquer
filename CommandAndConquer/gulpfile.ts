5cbc7ae341361ce139207365e25bcb89a4925036
EvAlex <evalexhimself@gmail.com>
2015-10-25 12:49:27 +0300

    added post-build gulp task to browserify

---
+++ CommandAndConquer/gulpfile.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="typings/tsd.d.ts" />
+
+import gulp = require('gulp');
+import browserify = require('gulp-browserify');
+import rename = require('gulp-rename');
+
+gulp.task('default', () => {
+    return gulp.src(['./Web/**/*.js', '!./Web/releases/**/*.js'])
+        .pipe(browserify({}))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('./Web'));
+});
